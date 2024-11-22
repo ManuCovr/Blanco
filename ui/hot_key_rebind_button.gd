651d4e3b6ad259ad84b0432451df13b2dcb37f92
ManuCovr <mtmpfb@gmail.com>
2024-11-22 11:29:17 +0000

    Changeable KeyBind

--- ui/hot_key_rebind_button.gd
+++ ui/hot_key_rebind_button.gd
@@ -11,6 +11,7 @@ class_name HotKeyRebindButton
 func _ready() -> void:
 	set_process_unhandled_input(false)
 	set_action_name()
+	set_text_for_key()
 	
 func set_action_name() -> void:
 	label.text = "Unassigned"
@@ -29,3 +30,46 @@ func set_action_name() -> void:
 			label.text = "Melee Attack"
 		"interact":
 			label.text = "Interact"
+
+func set_text_for_key() -> void:
+	var action_events = InputMap.action_get_events(action_name)
+	var action_event = action_events[0]
+	var action_keycode = OS.get_keycode_string(action_event.physical_keycode)
+	
+	button.text = "%s" % action_keycode
+
+
+func _on_button_toggled(button_pressed):
+	if button_pressed:
+		button.text = "Press any key..."
+		set_process_unhandled_key_input(button_pressed)
+		for i in get_tree().get_nodes_in_group("hotkey_button"):
+			if i.action_name != self.action_name:
+				i.button.toggle_mode = false
+				i.set_process_unhandled_key_input(false)
+	else:
+		for i in get_tree().get_nodes_in_group("hotkey_button"):
+			if i.action_name != self.action_name:
+				i.button.toggle_mode = true
+				i.set_process_unhandled_key_input(false)
+		set_text_for_key()
+	
+func _unhandled_key_input(event):
+	rebind_action_key(event)
+	button.button_pressed = false
+	
+func rebind_action_key(event):
+	var is_duplicate=false
+	var action_event=event
+	var action_keycode=OS.get_keycode_string(action_event.physical_keycode)
+	for i in get_tree().get_nodes_in_group("hotkey_button"):
+			if i.action_name!=self.action_name:
+				if i.button.text=="%s" %action_keycode:
+					is_duplicate=true
+					break
+	if not is_duplicate:
+		InputMap.action_erase_events(action_name)
+		InputMap.action_add_event(action_name,event)
+		set_process_unhandled_key_input(false)
+		set_text_for_key()
+		set_action_name()
